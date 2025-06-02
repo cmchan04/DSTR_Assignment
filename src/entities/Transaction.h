05e30bf6ca9c0468a9ec4ec94712839baf3c98b9
CM Chan <chunming04@gmail.com>
2025-06-02 20:59:55 +0800

    Revert "Function to read file into Array has completed in main"
    This reverts commit 54590884d10bada35fe24d50e9b61e6df85a1e69.

--- src/entities/Transaction.h
+++ src/entities/Transaction.h
@@ -1,33 +1,14 @@
 //
-// Created by cmchan04 on 6/2/2025.
+// Created by CM on 6/2/2025.
 //
 
 #include <string>
 #pragma once
 
-using namespace std;
-
 struct Transaction {
 
     //Declaration of variables
-    string transactionId;
-    string timestamp;
-    string senderAcc;
-    string receiverAcc;
-    double amount;
-    string transactionType;
-    string merchantCategory;
-    string location;
-    string deviceUsed;
-    bool isFraud;
-    string fraudType;
-    double timeSinceLastTransaction;
-    double spendingDeviationScore;
-    int velocityScore;
-    double geoAnomalyScore;
-    string paymentChannel;
-    string ipAddress;
-    string deviceHash;
+
 
 };
 
